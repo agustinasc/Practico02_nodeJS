3b0ffd68e0d0ce29baf16aac4e7e46fe34c6ea49
agustinasc <agustina.santillancastro@gmail.com>
2024-10-17 23:50:13 -0300

    Practico 02

---
+++ os-ejemplo/index.mjs
@@ -0,0 +1,12 @@
+import os from 'os';
+
+console.log('Arquitectura:', os.arch());
+
+console.log('Plataforma:',  os.platform());
+
+console.log('Memoria Total', os.totalmem());
+
+console.log('Informacion de la CPU:', os.opus());
+
+
+
